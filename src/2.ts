1e8eac37057bc4bfde044f2574322cb7fc14fb58
DmShko <vombat_schev@i.ua>
2023-11-16 17:41:10 +0200

    goit-typescript-hw-03-01

--- src/2.ts
+++ src/2.ts
@@ -16,9 +16,9 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+  public name: string;
+  private department: string;
+  protected salary: number;
 
   constructor(name: string, department: string, salary: number) {
     this.name = name;
@@ -33,7 +33,19 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number){
+    super(name, department, salary);
+  }
+
+  changeSalary(data: number) {
+    this.salary += data;
+    // console.log(this.salary);
+  }
+  
 }
 
+const m = new Manager('Jack', 'New York', 0);
+
+m.changeSalary(10000);
 
 export {};
